4cd07c79a66860c5cd16e3f1d893e5b23ebe851a
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2025-05-18 22:40:18 +0200

    use LazyVStack with ScrollView and Section (header/footer)

---
+++ SwiftUI_Views/SwiftUI_Views/LayoutViews/LazyVStack/LazyVStack_WithScrolling.swift
@@ -0,0 +1,18 @@
+//
+//  LazyVStack_WithScrolling.swift
+//  SwiftUI_Views
+//
+//  Created by Mohsin Ali Ayub on 18.05.25.
+//
+
+import SwiftUI
+
+struct LazyVStack_WithScrolling: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LazyVStack_WithScrolling()
+}
